982fc27001777a69a09ef9dd6835f4b174afa58a
4rkal <4rkal@proton.me>
2023-06-25 11:15:32 +0300

    archuseriso

---
+++ airootfs/root/customize_airootfs.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+set -e -u
+
+sed -i 's/#\(en_US\.UTF-8\)/\1/' /etc/locale.gen
+locale-gen
+
+sed -i "s/#Server/Server/g" /etc/pacman.d/mirrorlist
